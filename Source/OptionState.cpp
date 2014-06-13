e51ecef3349bf3e5af5908019726d76ff7ff9f8c
tylersandman <tylerbtbam@gmail.com>
2014-06-13 19:09:26 -0400

    Added default resolution option selection

--- Source/OptionState.cpp
+++ Source/OptionState.cpp
@@ -62,24 +62,40 @@ OptionState::OptionState(StateStack &stack, Context context) :
 
     auto resolutionOption = std::make_shared<GUI::VectorOption>(
         "Resolution", *getContext().fontManager);
-    resolutionOption->addPair(std::make_pair("800 x 600", sf::Vector2f(800, 600)));
-    resolutionOption->addPair(std::make_pair("1024 x 768", sf::Vector2f(1024, 768)));
-    resolutionOption->addPair(std::make_pair("1152 x 648", sf::Vector2f(1152, 648)));
-    resolutionOption->addPair(std::make_pair("1152 x 864", sf::Vector2f(1152, 864)));
-    resolutionOption->addPair(std::make_pair("1280 x 720", sf::Vector2f(1280, 720)));
-    resolutionOption->addPair(std::make_pair("1280 x 800", sf::Vector2f(1280, 800)));
-    resolutionOption->addPair(std::make_pair("1280 x 960", sf::Vector2f(1280, 960)));
-    resolutionOption->addPair(std::make_pair("1280 x 1024", sf::Vector2f(1280, 1024)));
-    resolutionOption->addPair(std::make_pair("1360 x 768", sf::Vector2f(1360, 768)));
-    resolutionOption->addPair(std::make_pair("1360 x 1024", sf::Vector2f(1360, 1024)));
-    resolutionOption->addPair(std::make_pair("1366 x 768", sf::Vector2f(1366, 768)));
-    resolutionOption->addPair(std::make_pair("1400 x 1050", sf::Vector2f(1400, 1050)));
-    resolutionOption->addPair(std::make_pair("1440 x 900", sf::Vector2f(1440, 900)));
-    resolutionOption->addPair(std::make_pair("1600 x 900", sf::Vector2f(1600, 900)));
-    resolutionOption->addPair(std::make_pair("1600 x 1200", sf::Vector2f(1600, 1200)));
-    resolutionOption->addPair(std::make_pair("1680 x 1050", sf::Vector2f(1680, 1050)));
-    resolutionOption->addPair(std::make_pair("1776 x 1000", sf::Vector2f(1776, 1000)));
-    resolutionOption->addPair(std::make_pair("1920 x 1080", sf::Vector2f(1920, 1080)));
+    std::vector<std::pair<std::string, sf::Vector2f>> resolutionOptionPairs;
+    resolutionOptionPairs.push_back(std::make_pair("800 x 600", sf::Vector2f(800, 600)));
+    resolutionOptionPairs.push_back(std::make_pair("1024 x 768", sf::Vector2f(1024, 768)));
+    resolutionOptionPairs.push_back(std::make_pair("1152 x 648", sf::Vector2f(1152, 648)));
+    resolutionOptionPairs.push_back(std::make_pair("1152 x 864", sf::Vector2f(1152, 864)));
+    resolutionOptionPairs.push_back(std::make_pair("1280 x 720", sf::Vector2f(1280, 720)));
+    resolutionOptionPairs.push_back(std::make_pair("1280 x 800", sf::Vector2f(1280, 800)));
+    resolutionOptionPairs.push_back(std::make_pair("1280 x 960", sf::Vector2f(1280, 960)));
+    resolutionOptionPairs.push_back(std::make_pair("1280 x 1024", sf::Vector2f(1280, 1024)));
+    resolutionOptionPairs.push_back(std::make_pair("1360 x 768", sf::Vector2f(1360, 768)));
+    resolutionOptionPairs.push_back(std::make_pair("1360 x 1024", sf::Vector2f(1360, 1024)));
+    resolutionOptionPairs.push_back(std::make_pair("1366 x 768", sf::Vector2f(1366, 768)));
+    resolutionOptionPairs.push_back(std::make_pair("1400 x 1050", sf::Vector2f(1400, 1050)));
+    resolutionOptionPairs.push_back(std::make_pair("1440 x 900", sf::Vector2f(1440, 900)));
+    resolutionOptionPairs.push_back(std::make_pair("1600 x 900", sf::Vector2f(1600, 900)));
+    resolutionOptionPairs.push_back(std::make_pair("1600 x 1200", sf::Vector2f(1600, 1200)));
+    resolutionOptionPairs.push_back(std::make_pair("1680 x 1050", sf::Vector2f(1680, 1050)));
+    resolutionOptionPairs.push_back(std::make_pair("1776 x 1000", sf::Vector2f(1776, 1000)));
+    resolutionOptionPairs.push_back(std::make_pair("1920 x 1080", sf::Vector2f(1920, 1080)));
+    
+    for (auto &pair : resolutionOptionPairs)
+        resolutionOption->addPair(pair);
+
+    sf::Vector2f resolution = sf::Vector2f(
+        sf::VideoMode::getDesktopMode().width,
+        sf::VideoMode::getDesktopMode().height);
+    
+    resolutionOption->select(0);
+    for (int i = 0; i < resolutionOptionPairs.size(); ++i){
+        if ((resolution.x == resolutionOptionPairs[i].second.x) && 
+            (resolution.y == resolutionOptionPairs[i].second.y))
+            resolutionOption->select(i);
+    }
+
     resolutionOption->setCallback(
         [this](sf::Vector2f resolution){
             getContext().window->create(sf::VideoMode(
